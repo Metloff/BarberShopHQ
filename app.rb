30be02dc7c17fd0e59235d0ffb48d6ffa65b888c
Metloff <genrih.metlov@gmail.com>
2015-10-16 19:30:55 +0300

    Add /visit + js plugins

--- app.rb
+++ app.rb
@@ -12,6 +12,31 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.order 'created_at DESC'
+end
+
 get '/' do
+	@barbers = Barber.order 'created_at DESC'
 	erb :index 
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+	
+	c = Client.new
+	c.name = @username
+	c.phone = @phone
+	c.datestamp = @datetime
+	c.barber = @barber 
+	c.color = @color
+	c.save
 end
